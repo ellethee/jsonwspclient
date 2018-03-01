46bc39fbe08c37f82b94ea93bdc012ed7d2dc792
ellethee <luca800@gmail.com>
2018-03-01 12:02:22 +0100

    Celanup

--- jsonwspclient/jsonwsputils.py
+++ jsonwspclient/jsonwsputils.py
@@ -25,24 +25,6 @@ _get_charset = re.compile(
     r'(?i)charset\s*=\s*(?P<charset>[-_.a-zA-Z0-9]+)').search
 log = logging.getLogger('jsonwspclient')
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in
-    this function because it is programmed to be pretty
-    printed and may differ from the actual request.
-    """
-    log.debug('{}\n{}\n{}\n\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
-
-
-
 def get_fileitem(path, data='data', name='name', mode='rb'):
     """get fileitem"""
     path = os.path.abspath(path)
