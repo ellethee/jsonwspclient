b3e3508599810848035f41df536bd8479659a1b3
ellethee <luca800@gmail.com>
2018-03-08 11:01:04 +0100

    Added context manager

--- jsonwspclient/jsonwspclient.py
+++ jsonwspclient/jsonwspclient.py
@@ -200,6 +200,12 @@ class JsonWspClient(object):
         return sorted(self.__dict__.keys() + self._methods.keys() +
                       self._services.keys())
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
     def __del__(self):
         self.session.close()
         del self.session
